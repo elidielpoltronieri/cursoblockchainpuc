a91d5bd43bb7311c7e9e0c1de7539728abaadcf0
elidielpoltronieri <56139039+elidielpoltronieri@users.noreply.github.com>
2019-10-29 21:19:01 -0300

    Update guardaloteria1.sol

--- guardaloteria1.sol
+++ guardaloteria1.sol
@@ -1,55 +1,60 @@
 pragma solidity 0.5.12;
 
 contract GuardaLoteria {
+    
     uint numeroSorteado;
     address dono;
     uint contadorDeSorteios = 0;
-    bool DonoRico = false;
+    bool donoRico = false;
     
     constructor (uint numeroInicial) public {
-    require (msg.sender.balance > 99,99999999999 ether);
-    
-    numeroSorteado = numeroInicial;
-    dono = msg.sender;
-    contadorDeSorteios = 1;
-    
-    if (msg.sender.balance > 20 ether) {
-    donoRico = true;
-    }
-    else {
-    donoRico = false;
-    }
-    }
-    
-    function set(uint enviado) public payable comCustoMinimo(1000){
-    nuemroSorteado = enviado;
-    contadorDeSorteios ++;
-    
-    if (msg.value > 1000){
-    uint troco = msg.value - 1000;
-    msg.sender.transfer (troco);
-    emit trocoEnviado(msg.sender, troco);
-    }
+        
+        numeroSorteado = numeroInicial;
+        dono = msg.sender;
+        contadorDeSorteios = 1;
+        if (msg.sender.balance > 20000000 finney) {
+            donoRico = true;
+        }
+        else {
+            donoRico = false;
+        }
     }
     
     modifier comCustoMinimo (uint min) {
-    require(msg.value >= min, "Não foi enviado Ether suficiente.");
-    _;
+        require(msg.value >= min, "Não foi enviado Ether suficiente.");
+        _;
     }
     
+    function set(uint enviado) public payable comCustoMinimo(1000) {
+        numeroSorteado = enviado;
+        contadorDeSorteios ++;
+        
+        if (msg.value > 1000) {
+            uint troco = msg.value - 1000;
+            msg.sender.transfer (troco);
+            emit trocoEnviado(msg.value);
+        }
+    }
+    
+    event trocoEnviado (uint troco);
+    
+     function saldoNoContrato () public view returns (uint) {
+        return address(this).balance;
+     }
+    
     function get () public view returns (uint) {
-    return numeroSorteado;
+        return numeroSorteado;
     }
     
     function contador() public view returns (uint){
-    return contadorDeSorteios;
+        return contadorDeSorteios;
     }
     
     function Dono () public view returns (address) {
-    return dono;
+        return dono;
     }
     
     function Rico () public view returns (bool){
-    return donoRico;
+        return donoRico;
     }
 }
